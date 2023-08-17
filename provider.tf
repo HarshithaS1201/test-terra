1a087732544d2160cff6ddf547a47f7bb0a7913c
HarshithaS1201 <133198952+HarshithaS1201@users.noreply.github.com>
2023-08-17 13:31:51 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
-provider "github" {
-  token ="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"
-}
-resource "github_repository" "example" {
-  name        = "TerraformJenkinsDemo"
-  description = "My awesome codebase for Terraform and jenkins"
-
-  visibility = "public"
-  auto_init = true
-}
-
-provider "aws" {
-  region = "us-east-1"
-  profile = "default"
-}
+#provider "github" {
+#  token ="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"
+#}
+#resource "github_repository" "example" {
+#  name        = "TerraformJenkinsDemo"
+#  description = "My awesome codebase for Terraform and jenkins"
+#
+#  visibility = "public"
+#  auto_init = true
+#}
+#
+#provider "aws" {
+#  region = "us-east-1"
+#  profile = "default"
+#}
