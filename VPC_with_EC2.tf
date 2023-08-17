d672179362c4b628d7e352a585c1021e0ac37196
HarshithaS1201 <133198952+HarshithaS1201@users.noreply.github.com>
2023-08-17 13:34:32 +0530

    Add files via upload

--- VPC_with_EC2.tf
+++ VPC_with_EC2.tf
@@ -1,13 +1,13 @@
 provider "github" {
   token ="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"
 }
-#resource "github_repository" "example" {
-#  name        = "TerraformJenkinsDemo"
-#  description = "My awesome codebase for Terraform and jenkins"
-#
-#  visibility = "public"
-#  auto_init = true
-#}
+resource "github_repository" "example" {
+  name        = "test-terra"
+  description = "My awesome codebase for Terraform and jenkins"
+
+  visibility = "public"
+  auto_init = true
+}
 
 provider "aws" {
   region = "us-east-1"
